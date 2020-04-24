fdce9d600d572f7477a41de648b1641c1715bc06
André Joaquim Bez Birolo <62669640+AndreBezBirolo@users.noreply.github.com>
2020-04-24 17:18:00 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+new SimpleSlide({
+    slide: "principal-1", // nome do atributo data-slide="principal"
+    nav: false, // se deve ou não mostrar a navegação
+    auto: true, // se o slide deve passar automaticamente
+    time: 5000, // tempo de transição dos slides
+    pauseOnHover: true // pausa a transição automática
+  });    
+
